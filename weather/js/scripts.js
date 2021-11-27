33b42ce0dbe0abdec8c632040792f9afb357d232
liaayos <ayo21002@byui.edu>
2021-11-27 00:15:26 -0500

    added storm center page

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -13,3 +13,14 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
+function adjustRating(rating) {
+  document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+const s = document.querySelector('#selected')
+const sel = document.querySelector('#selectbrowser');
+s.style.display = "block";
+s.textContent = sel.value;
+
+}
